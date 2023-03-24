e61d5fd3849a2677b8ab3bd6a85f7a7adc0ba8bb
hyochan <dooboolab@gmail.com>
2023-03-24 22:26:13 +0900

    chore: set missing locale config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  i18n: {
+    locales: ['en', 'ko'],
+    defaultLocale: 'en',
+  },
   swcMinify: true,
   reactStrictMode: true,
   experimental: {appDir: true},
