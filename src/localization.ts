5e74819b55d61e041e027240829419a30c90a158
KIHUN KIM <kihunkim@KIHUNui-MacBookAir.local>
2023-06-18 14:33:18 +0900

    Update localization for variable

--- src/localization.ts
+++ src/localization.ts
@@ -16,10 +16,37 @@ export type Translation = {
   [K in keyof Translates]: Translates[K];
 };
 
+type LocalizationVariables = (
+  text: string,
+  params?: Record<string, string | number>,
+) => string;
+
+const variableFormatter = (): LocalizationVariables => {
+  return (text, params) => {
+    if (!params) {
+      return text;
+    }
+
+    const result = Object.entries(params).reduce((acc, [variable, value]) => {
+      const pattern = new RegExp(`{{\\s*${variable}\\s*}}`, 'g');
+
+      return acc.replace(pattern, String(value));
+    }, text);
+
+    return result;
+  };
+};
+
 export const getTranslates = async (locale: Locale): Promise<Translates> => {
   if (typeof translates[locale] !== 'function') {
     return translates['en']();
   }
 
-  return translates[locale]();
+  const translation = await translates[locale]();
+  const formattedTranslation = {
+    ...translation,
+    t: variableFormatter(),
+  };
+
+  return formattedTranslation;
 };
