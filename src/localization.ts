0b558d6c47b71d65bb7e3f240e12fd904642c540
Jay <deutschkihun@gmail.com>
2023-06-24 23:29:45 +0900

    feat: add localization with variable (#5)
    When you want to need insert `variable` in your localization text,
    please use `t`. Wrap the target object with `t` and add key-value pair.
    
    - related issue:
    https://github.com/prisma-korea/prisma-nextjs-rsc/issues/3
    
    ---------
    
    Co-authored-by: KIHUN KIM <kihunkim@KIHUNui-MacBookAir.local>

--- src/localization.ts
+++ src/localization.ts
@@ -2,7 +2,7 @@ import type {Locale} from './i18n';
 
 // We enumerate all dictionaries here for better linting and typescript support
 // We also get the default import for cleaner types
-const translates = {
+const translates: Record<Locale, any> = {
   en: () =>
     import('../assets/locales/en.json').then((module) => module.default),
   ko: () =>
@@ -16,10 +16,37 @@ export type Translation = {
   [K in keyof Translates]: Translates[K];
 };
 
+type LocalizationVariables = (
+  text: string,
+  params?: Record<string, string | number>,
+) => string;
+
+const variableFormatter = (): LocalizationVariables => {
+  return (text, params) => {
+    if (!params) {
+      return text;
+    }
+
+    const result = Object.entries(params).reduce((acc, [variable, value]) => {
+      const pattern = new RegExp(`{{\\s*${variable}\\s*}}`, 'g');
+
+      return acc.replace(pattern, String(value));
+    }, text);
+
+    return result;
+  };
+};
+
 export const getTranslates = async (locale: Locale): Promise<Translates> => {
   if (typeof translates[locale] !== 'function') {
     return translates['en']();
   }
 
-  return translates[locale]();
+  const translation = await translates[locale]();
+  const formattedTranslation = {
+    ...translation,
+    t: variableFormatter(),
+  };
+
+  return formattedTranslation;
 };
